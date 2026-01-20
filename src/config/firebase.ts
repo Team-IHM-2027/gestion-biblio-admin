6768a1cefcbf1f48efffb726d3cd2710820d7ddf
ArreyntowKerone <1enowarreyntow@gmail.com>
2026-01-20 16:28:35 +0100

    Trigger redeploy with correct Firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getFunctions } from 'firebase/functions';
-
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -20,7 +20,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-
+export const analytics = getAnalytics(app);
 
 export const functions = getFunctions(app);
 
